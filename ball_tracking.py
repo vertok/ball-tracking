e5a7cbeb3bd2f14f46d32222dc405c55070995ae
vertok <vertok@hotmail.com>
2021-04-16 23:11:20 +0200

    added

--- ball_tracking.py
+++ ball_tracking.py
@@ -4,12 +4,22 @@ import argparse
 import imutils
 import cv2
 
+def nothing(x):
+    pass
+
+# define the default trackbar to find the right color range
+cv2.namedWindow("Trackbars")
+
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the (optional) video file")
 args = vars(ap.parse_args())
-
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
 
 if not args.get("video", False):
     camera = cv2.VideoCapture(0)
@@ -17,6 +27,18 @@ else:
     camera = cv2.VideoCapture(args["video"])
 
 while True:
+    # adding trackbar to find the right color range
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+
+    # assign values from trackbar to blueLower/ blueUpper
+    blueLower = (l_h, l_s, l_v)
+    blueUpper = (u_h, u_s, u_v)
+
     (grabbed, frame) = camera.read()
 
     if args.get("video") and not grabbed:
@@ -25,7 +47,7 @@ while True:
     frame = imutils.resize(frame, width=600)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.inRange(hsv, blueLower, blueUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
@@ -36,11 +58,15 @@ while True:
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
 
-        if radius > 10:
+        if radius > 1:
             cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+
+    # as output here there will be only the subject OIin color
+    result = cv2.bitwise_and(frame, frame, mask=mask)
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
+    cv2.imshow("Result", result)
 
     key = cv2.waitKey(1) & 0xFF
 
